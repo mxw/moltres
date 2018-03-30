15407e162594665f97b0c2c1a8c179201c3c7359
Max Wang <mxawng@gmail.com>
2018-03-29 23:18:58 -0400

    Implement raid support; lots of refactoring

--- moltres.js
+++ moltres.js
@@ -51,13 +51,86 @@ process.on('SIGABRT', signal_handler);
 
 ///////////////////////////////////////////////////////////////////////////////
 
-const cmds = new Set([
-  'gym', 'add-gym', 'edit-gym', 'rm-gym',
-]);
+const Permission = {
+  ADMIN: 0,
+  NONE: 1,
+  TABLE: 2,
+};
+
+const cmd_order = [
+  'help',
+  'gym', 'add-gym',
+  'raid', 'spot-egg', 'spot-raid',
+];
+
+const cmds = {
+  help: {
+    perms: Permission.NONE,
+    usage: '[request]',
+    args: [0, 1],
+    desc: 'Learn about our team\'s legendary avatar.',
+  },
+  gym: {
+    perms: Permission.NONE,
+    usage: '<handle>',
+    args: [1, 1],
+    desc: 'Get information about a gym.',
+  },
+  'add-gym': {
+    perms: Permission.TABLE,
+    usage: '<handle> <region> <lat> <lng> <name>',
+    args: [5, 100],
+    desc: 'Add a new gym to the roost.',
+  },
+  raid: {
+    perms: Permission.NONE,
+    usage: '<gym-handle>',
+    args: [1, 1],
+    desc: 'Get information about the current raid at a gym.',
+  },
+  'spot-egg': {
+    perms: Permission.NONE,
+    usage: '<gym-handle> <tier> <MM:SS-til-hatch>',
+    args: [3, 3],
+    desc: 'Announce a raid egg.'
+  },
+  'spot-raid': {
+    perms: Permission.NONE,
+    usage: '<gym-handle> <boss> <MM:SS-til-despawn>',
+    args: [3, 3],
+    desc: 'Announce a hatched raid boss.'
+  },
+};
 
 const unrestricted_cmds = new Set([
   'gym',
 ]);
+
+const raid_tiers = {
+  snorunt: 1,
+  swablu: 1,
+  wailmer: 1,
+  magikarp: 1,
+
+  manectric: 2,
+  mawile: 2,
+  sableye: 2,
+  electabuzz: 2,
+  exeggutor: 2,
+
+  piloswine: 3,
+  jolteon: 3,
+  jynx: 3,
+  gengar: 3,
+  machamp: 3,
+
+  absol: 4,
+  aggron: 4,
+  tyranitar: 4,
+  golem: 4,
+
+  lugia: 5,
+};
 
 ///////////////////////////////////////////////////////////////////////////////
 // Discord utilities.
@@ -65,8 +138,9 @@ const unrestricted_cmds = new Set([
 /*
  * Avoid polluting the rest of the file with emoji.
  */
-const emoji = {
+const emoji_by_name = {
   no_good: '🙅',
+  no_entry_sign: '🚫',
 };
 
 /*
@@ -74,6 +148,20 @@ const emoji = {
  */
 function get_emoji(name) {
   return moltres.emojis.find('name', name);
+}
+
+/*
+ * Add reactions to `msg' in order.
+ */
+function chain_reaccs(msg, ...reaccs) {
+  if (reaccs.length === 0) return;
+  let [head, ...tail] = reaccs;
+
+  let emoji = emoji_by_name[head] || get_emoji(head);
+
+  msg.react(emoji)
+  .then(r => { chain_reaccs(r.message, ...tail); })
+  .catch(console.error);
 }
 
 /*
@@ -110,11 +198,19 @@ function react_success(msg, reacc = null) {
   log_impl(msg, null, reacc || get_emoji('approved'));
 };
 function log_invalid(msg, str, reacc = null) {
-  log_impl(msg, str, reacc || emoji.no_good);
+  log_impl(msg, str, reacc || emoji_by_name.no_good);
 };
 function log_error(msg, str, reacc = null) {
-  log_impl(msg, str, reacc || emoji.no_good);
+  log_impl(msg, str, reacc || emoji_by_name.no_good);
 };
+
+/*
+ * Get the usage string for `cmd'.
+ */
+function usage_string(cmd) {
+  if (!(cmd in cmds)) return null;
+  return `Usage: \`${cmd} ${cmds[cmd].usage}\``;
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 // MySQL utilities.
@@ -133,14 +229,38 @@ function errwrap(fn = null) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+// General handlers.
+
+function handle_help(msg, args) {
+  let out = null;
+
+  if (args.length === 0) {
+    out = get_emoji('valor') +
+          '  Please type `:>` followed by your request:\n\n';
+    for (let cmd of cmd_order) {
+      out += `\`${cmd}\`:  ${cmds[cmd].desc}\n`;
+    }
+  } else {
+    let [cmd] = args;
+    out = `\`${cmd}\`:  ${cmds[cmd].desc}\n${usage_string(cmd)}`;
+  }
+  msg.channel.send(out.trim());
+}
+
+///////////////////////////////////////////////////////////////////////////////
 // Gym handlers.
 
-function handle_gym(msg, args) {
-  const usage = 'Usage: gym <handle>';
+/*
+ * Stringify a row from the gyms table.
+ */
+function gym_row_to_string(msg, gym) {
+  return `\`[${gym.handle}]\`
+name: **${gym.name}**
+region: ${msg.guild.roles.get(gym.region).name}
+coords: <https://maps.google.com/maps?q=${gym.lat},${gym.lng}>`;
+}
 
-  if (args.length !== 1) {
-    return log_invalid(msg, usage);
-  }
+function handle_gym(msg, args) {
   let [handle] = args;
 
   conn.query(
@@ -151,14 +271,9 @@ function handle_gym(msg, args) {
       if (results.length !== 1) {
         return log_invalid(msg, `Multiple gyms matching ${handle}.`);
       }
-
       let [gym] = results;
 
-      msg.channel.send(`\`[${gym.handle}]\`
-name: **${gym.name}**
-region: ${msg.guild.roles.get(gym.region).name}
-coords: <https://maps.google.com/maps?q=${gym.lat},${gym.lng}>`
-      ).then(m => {
+      msg.channel.send(gym_row_to_string(msg, gym)).then(m => {
         log_success(msg, `Handled \`gym\` from ${msg.author.tag}.`);
       }).catch(console.error);
     })
@@ -166,10 +281,8 @@ coords: <https://maps.google.com/maps?q=${gym.lat},${gym.lng}>`
 }
 
 function handle_add_gym(msg, args) {
-  const usage = 'Usage: add-gym <handle> <region> <lat> <lng> <name>';
-
-  if (args.length < 5 || total_mentions(msg) !== 1) {
-    return log_invalid(msg, usage);
+  if (total_mentions(msg) !== 1) {
+    return log_invalid(msg, usage_string('add-gym'));
   }
   let [handle, region, lat, lng, ...name] = args;
 
@@ -201,16 +314,153 @@ function handle_rm_gym(msg, args) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+// Raid handlers.
+
+/*
+ * Extract the minutes and seconds from a raid countdown timer.
+ */
+function parse_timer(timer) {
+  let matches = timer.match(/^(\d{1,2}):(\d\d)$/);
+  if (matches === null) return null;
+
+  return {
+    mins: parseInt(matches[1]),
+    secs: parseInt(matches[2]),
+  };
+}
+
+/*
+ * Stringify a Date object according to our whims.
+ */
+function time_to_string(date) {
+  return date.toLocaleString('en-US', {
+    timeZone: 'America/New_York',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  });
+}
+
+function handle_raid(msg, args) {
+  let [handle] = args;
+
+  conn.query(
+    'SELECT * FROM gyms INNER JOIN raids ON gyms.id = raids.gym_id ' +
+    'WHERE handle LIKE ?',
+    [`%${handle}%`],
+
+    errwrap(function (results, fields) {
+      if (results.length !== 1) {
+        chain_reaccs(msg, 'no_entry_sign', 'RaidEgg');
+      }
+      let [raid] = results;
+
+      let now = new Date(Date.now());
+
+      if (raid.despawn < now) {
+        console.log('yo');
+        conn.query(
+          'DELETE FROM raids WHERE gym_id = ?',
+          [raid.gym_id],
+          errwrap()
+        );
+        return chain_reaccs(msg, 'no_entry_sign', 'RaidEgg');
+      }
+
+      let hatch = new Date(raid.despawn.getTime());
+      hatch.setMinutes(hatch.getMinutes() - 45);
+
+      let output = gym_row_to_string(msg, raid) + '\n';
+
+      if (now >= hatch) {
+        output +=`
+raid: ${raid.boss === null ? 'unknown' : raid.boss} (T${raid.tier})
+despawn: ${time_to_string(raid.despawn)}`;
+      } else {
+        output +=`
+raid egg: T${raid.tier}
+hatch: ${time_to_string(hatch)}`;
+      }
+
+      msg.channel.send(output).then(m => {
+        log_success(msg, `Handled \`raid\` from ${msg.author.tag}.`);
+      }).catch(console.error);
+    })
+  );
+}
+
+function handle_spot(msg, handle, tier, boss, timer) {
+  timer = parse_timer(timer);
+  if (timer === null) {
+    return log_invalid(msg, `Invalid timer ${timer}.`);
+  }
+
+  let egg_adjust = boss === null ? 45 : 0;
+
+  let despawn = new Date(Date.now());
+  despawn.setMinutes(despawn.getMinutes() + timer.mins + egg_adjust);
+  despawn.setSeconds(despawn.getSeconds() + timer.secs);
+
+  let pop = new Date(despawn.getTime());
+  pop.setMinutes(pop.getMinutes() - 60 - 45);
+
+  let foo = conn.query(
+    'REPLACE INTO raids (gym_id, tier, boss, despawn, spotter) ' +
+    '   SELECT gyms.id, ?, ?, ?, ? FROM gyms ' +
+    '   WHERE ' +
+    '     gyms.handle LIKE ? ' +
+    '   AND ' +
+    '     NOT EXISTS ( ' +
+    '       SELECT * FROM raids ' +
+    '         WHERE gym_id = gyms.id ' +
+    '         AND despawn > ? ' +
+    '     ) ',
+    [tier, boss, despawn, msg.author.id, `%${handle}%`, pop],
+    errwrap((..._) => react_success(msg))
+  );
+  console.log(foo.sql);
+}
+
+function handle_spot_egg(msg, args) {
+  let [handle, tier, timer] = args;
+
+  handle_spot(msg, handle, tier, null, timer);
+}
+
+function handle_spot_raid(msg, args) {
+  let [handle, boss, timer] = args;
+  boss = boss.toLowerCase();
+
+  if (!(boss in raid_tiers)) {
+    return log_invalid(msg, `Unrecognized raid boss ${boss}.`);
+  }
+
+  handle_spot(msg, handle, raid_tiers[boss], boss, timer);
+}
+
+///////////////////////////////////////////////////////////////////////////////
 
 /*
  * Do the work of `request'.
  */
 function handle_request(msg, request, args) {
+  let params_range = cmds[request].args;
+
+  if (args.length < params_range[0] || args.length > params_range[1]) {
+    return log_invalid(msg, usage_string(request));
+  }
+
   switch (request) {
+    case 'help':      return handle_help(msg, args);
+
     case 'gym':       return handle_gym(msg, args);
     case 'add-gym':   return handle_add_gym(msg, args);
     case 'edit-gym':  return handle_edit_gym(msg, args);
     case 'rm-gym':    return handle_rm_gym(msg, args);
+
+    case 'raid':      return handle_raid(msg, args);
+    case 'spot-egg':  return handle_spot_egg(msg, args);
+    case 'spot-raid': return handle_spot_raid(msg, args);
     default:
       return log_invalid(msg, `Invalid request ${request}.`);
   }
@@ -223,10 +473,10 @@ function handle_request(msg, request, args) {
 function handle_request_with_check(msg, request, args) {
   let user_id = msg.author.id;
 
-  if (!cmds.has(request)) return;
+  if (!(request in cmds)) return;
 
   if (config.admin_ids.has(user_id) ||
-      unrestricted_cmds.has(request)) {
+      cmds[request].perms === Permission.NONE) {
     return handle_request(msg, request, args);
   }
 
